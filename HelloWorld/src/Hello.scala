3b88f45df0c6176f92db0f393f8c1ea9b35b9f70
Bharath D N <dnrbharath@Bharaths-MacBook-Pro.local>
2017-01-24 15:58:23 -0800

    First commit

---
+++ HelloWorld/src/Hello.scala
@@ -0,0 +1,14 @@
+
+
+object Hello extends App {
+  println("Hello World");
+}
+
+/*
+ * Another method to write above code 
+object Hello {
+  def main(args: Array[String]) {
+    println("Hello World");
+  }
+}
+*/
